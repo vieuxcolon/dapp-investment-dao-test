33a8e61c830a1d2359b5bd73806064fb2c98cd3f
vieuxcolon <vieuxcolon@hotmail.fr>
2026-01-18 09:43:33 +0100

    Update hardhat.config.ts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,12 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.21",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
-    }
-  }
+  solidity: "0.8.20",
 };
 
 export default config;
